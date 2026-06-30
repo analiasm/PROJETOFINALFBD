2f89c9ecf56ad4321531622557d871ae58320391
rayca-alves <annarayca1@gmail.com>
2026-06-30 15:52:39 -0300

    Update CRUDgrupo.py
    conexao

--- Trabalho Final/CRUDgrupo.py
+++ Trabalho Final/CRUDgrupo.py
@@ -1,1 +1,19 @@
+# --- CRUD ----
+import tkinter as tk
+from tkinter import ttk, messagebox
+import psycopg2 
 
+# --- conexão com o postgreSQL ---- 
+def obtener_conexao():
+    psycopg2.extensions.register_type(psycopg2.extensions.UNICODE)
+    psycopg2.extensions.register_type(psycopg2.extensions.UNICODEARRAY)
+    
+    conn = psycopg2.connect(
+        host="localhost",
+        database="Sistemas_GE", 
+        user="postgres",              
+        password="senha123",        
+        port="5432"                    
+    )
+    conn.set_client_encoding('LATIN1') # Mantido conforme seu padrão de encoding
+    return conn
